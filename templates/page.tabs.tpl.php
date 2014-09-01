4ea5f171baa4ab6a38cb3af8417eb689ed1b5db1
zacdavidm <zacdavidm@gmail.com>
2014-09-01 10:45:07 -0400

    update button group template and styles as well as menu and inline link styles

--- templates/page.tabs.tpl.php
+++ templates/page.tabs.tpl.php
@@ -1,6 +1,10 @@
 			<?php if (!empty($tabs)): ?>
-				<div class="action-tabs">
-					<?php print render($tabs); ?>
-					<?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+			<div class="row">
+				<div class="medium-12 columns">
+          <div class="action-tabs">
+            <?php print render($tabs); ?>
+            <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+          </div>
 				</div>
+			</div>
       <?php endif; ?>
